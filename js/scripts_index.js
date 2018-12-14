76023fe3b2f0c0bd0631526cf8504de3228d0304
Timo Lubitz <timo.lubitz@gmail.com>
2018-12-14 14:29:53 +0100

    !new layout for ridiculous requests!

--- js/scripts_index.js
+++ js/scripts_index.js
@@ -154,6 +154,30 @@ function showSlides2(n) {
   slides[slideIndex-1].style.display = "block";
 }
 
+/* Animation of logo flyer */
+$(document).ready(function(e) {
+    var width = $(document).width();
+
+    function goRight() {
+        $("#animate").animate({
+        left: width
+      }, 5000, function() {
+         setTimeout(goLeft, 50);
+      });
+    }
+    function goLeft() {
+        $("#animate").animate({
+        left: 0
+      }, 5000, function() {
+         setTimeout(goRight, 50);
+      });
+    }
+
+    setTimeout(goRight, 50);
+});
+
+
+
 /* SHOP ITEMS */
 /* 1 */
     (function () {
