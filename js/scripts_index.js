37bc6fa56a6dc849f635a9b454848668b67b2e08
Timo Lubitz <timo.lubitz@gmail.com>
2019-01-10 11:13:52 +0100

    fixed a bug in index js

--- js/scripts_index.js
+++ js/scripts_index.js
@@ -81,30 +81,29 @@ $(function(){
 
 
 /* Image gallery lightbox 1 (modal) */
-
 // Open the Modal
-function openModal() {
-  document.getElementById('myModal').style.display = "block";
-}
-
+//function openModal() {
+//  document.getElementById('myModal').style.display = "block";
+//}
+//
 // Close the Modal
-function closeModal() {
-  document.getElementById('myModal').style.display = "none";
-}
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
+//function closeModal() {
+//  document.getElementById('myModal').style.display = "none";
+//}
+//
+//var slideIndex = 1;
+//showSlides(slideIndex);
+//
 // Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
+//function plusSlides(n) {
+//  showSlides(slideIndex += n);
+//}
+//
 // Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
+//function currentSlide(n) {
+//  showSlides(slideIndex = n);
+//}
+/*
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
@@ -120,39 +119,39 @@ function showSlides(n) {
 /* Image gallery lightbox 2 (modal) */
 
 // Open the Modal
-function openModal2() {
-  document.getElementById('myModal2').style.display = "block";
-}
+//function openModal2() {
+//  document.getElementById('myModal2').style.display = "block";
+//}
 
 // Close the Modal
-function closeModal2() {
-  document.getElementById('myModal2').style.display = "none";
-}
-
-var slideIndex = 1;
-showSlides2(slideIndex);
-
+//function closeModal2() {
+//  document.getElementById('myModal2').style.display = "none";
+//}
+//
+//var slideIndex = 1;
+//showSlides2(slideIndex);
+//
 // Next/previous controls
-function plusSlides2(n) {
-  showSlides2(slideIndex += n);
-}
-
+//function plusSlides2(n) {
+//  showSlides2(slideIndex += n);
+//}
+//
 // Thumbnail image controls
-function currentSlide2(n) {
-  showSlides2(slideIndex = n);
-}
+//function currentSlide2(n) {
+//  showSlides2(slideIndex = n);
+//}
 
-function showSlides2(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides2");
-  var captionText = document.getElementById("caption2");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slides[slideIndex-1].style.display = "block";
-}
+//function showSlides2(n) {
+//  var i;
+//  var slides = document.getElementsByClassName("mySlides2");
+//  var captionText = document.getElementById("caption2");
+//  if (n > slides.length) {slideIndex = 1}
+//  if (n < 1) {slideIndex = slides.length}
+//  for (i = 0; i < slides.length; i++) {
+//    slides[i].style.display = "none";
+//  }
+//  slides[slideIndex-1].style.display = "block";
+//}
 
 /* Animation of logo flyer */
 $(document).ready(function(e) {
