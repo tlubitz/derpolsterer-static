0cb01c2e8edf425f5f2c57647eefd17877a008b0
Timo Lubitz <timo.lubitz@gmail.com>
2019-01-10 14:46:01 +0100

    adaptations for Manne

--- js/scripts_index.js
+++ js/scripts_index.js
@@ -17,7 +17,7 @@ function carousel() {
     if (slideIndex > x.length) {slideIndex = 1} 
     x[slideIndex-1].style.display = "block";
     /*y[slideIndex-1].style.display = "block"; */
-    setTimeout(carousel, 10000); // Change image every 5 seconds
+    setTimeout(carousel, 5000); // Change image every 5 seconds
 }
 
 /* gallery small manual slider MAIN */
